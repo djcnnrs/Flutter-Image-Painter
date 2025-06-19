7ce2154b9a4af81ff00847fcad6ef02cc7296d1e
Derek Connors <56665787+djcnnrs@users.noreply.github.com>
2025-06-19 14:55:23 -0400

    Expanded Compatibility Testing

--- lib/enhanced_image_painter.dart
+++ lib/enhanced_image_painter.dart
@@ -1,5 +1,7 @@
 // Enhanced Image Painter Package Export
 // Use this in your FlutterFlow custom widget with: import 'package:enhanced_image_painter/enhanced_image_painter.dart';
 
-// Absolute minimal export for FlutterFlow testing
+// Progressive export - adding back functionality step by step
 export 'basic_test.dart';
+export 'src/enhanced_controller.dart' show PaintMode, BackgroundType, PaintInfo;
+// Next: export 'src/enhanced_widget.dart';
